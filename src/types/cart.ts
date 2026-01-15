8c6c1c81ecc4c1da794c00881a5f6240a9955277
Richard NSANZIMANA <85228928+nsanzimanarichard1@users.noreply.github.com>
2026-01-15 15:40:02 +0200

    Delete src directory
    not needed

--- src/types/cart.ts
+++
@@ -1,7 +0,0 @@
-import type { Dessert } from "./dessert";
-
-export interface CartItem {
-  dessert: Dessert;
-  quantity: number;
-  addedAt: Date;
-}
--- src/utils/dom.ts
+++
@@ -1,7 +0,0 @@
-export function qs<T extends HTMLElement>(
-  selector: string
-): T {
-  const el = document.querySelector(selector);
-  if (!el) throw new Error(`Element not found: ${selector}`);
-  return el as T;
-}
